4ece5b49a541f82544ce92258a9063931cebfb86
Thiago Hersan <thiago.hersan+github@gmail.com>
2026-04-27 16:54:57 -0400

    new datasets for classification

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,9 +1,13 @@
 from io import BytesIO, StringIO
 from PIL import Image as PImage
 
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, confusion_matrix, root_mean_squared_error
+
 import csv
 import json
 import urllib.request as request
+
+from warnings import simplefilter
 
 def object_from_json_url(url):
   with request.urlopen(url) as response:
@@ -28,3 +32,7 @@ def list_from_csv_url(url):
           pass
 
     return list_data
+
+def display_confusion_matrix(labels, predicted, display_labels):
+  simplefilter(action='ignore', category=FutureWarning)
+  ConfusionMatrixDisplay.from_predictions(labels, predicted, display_labels=display_labels, xticks_rotation="vertical")
